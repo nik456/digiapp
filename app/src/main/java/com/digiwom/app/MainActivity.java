59c29546d5e65f2e39a0187034e3fecd37e5eaad
nik456 <53567910+nik456@users.noreply.github.com>
2022-03-17 14:45:22 +0200

    app
    app

---
+++ app/src/main/java/com/digiwom/app/MainActivity.java
@@ -0,0 +1,36 @@
+package com.digiwom.app;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
+
+    @Override
+    @SuppressLint("SetJavaScriptEnabled")
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWebView = findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.setWebViewClient(new MyWebViewClient());
+
+        // REMOTE RESOURCE
+        mWebView.loadUrl("https://www.digiwom.net/mobile?idx=33");
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mWebView.canGoBack()) {
+            mWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
